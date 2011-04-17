513578457c9319e7877922f1343b0b9420fcaf51
Michael Imamura <zoogie@lugatgt.org>
2011-04-17 17:36:45 -0400

    Add "About Android" slide.

--- content/src/org/lugatgt/zoogie/introtoandroid/MainPresentation.java
+++ content/src/org/lugatgt/zoogie/introtoandroid/MainPresentation.java
@@ -33,6 +33,7 @@ public class MainPresentation extends Presentation {
     private enum Slides implements Slide {
         
         TITLE(TitleSlide.class, "Intro to Android Development", Transitions.SLIDE_LEFT),
+        ANDROID(AndroidSlide.class, "About Android", Transitions.SLIDE_LEFT),
         STARTING(BasicTextSlide.class, "Getting Started", Transitions.SLIDE_LEFT, R.array.starting_content),
         DALVIK(BasicTextSlide.class, "Dalvik VM", Transitions.SLIDE_UP, R.array.dalvik_content),
         DALVIK_JAVA(BasicTextSlide.class, "Dalvik VM", "Where does Java fit in?", Transitions.SLIDE_LEFT, R.array.dalvik_java_content),
